ec45218496eb6153eea0a945d81d5cc16f5ab028
richardgmartin <richardgeorgemartin@gmail.com>
2016-01-12 15:43:39 -0600

    third commit

--- MySafariTNG/ViewController.m
+++ MySafariTNG/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController ()<UIWebViewDelegate, UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *variableWebView;
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 
 @end
 
@@ -19,12 +20,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self loadGivenURL:@"http://www.google.com"];
+    
 }
+
+-(void)webViewDidStartLoad:(UIWebView *)webView {
+    
+    [self.spinner startAnimating];
+    
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView {
+    
+    [self.spinner stopAnimating];
+    
+}
+
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    NSURL *url = [NSURL URLWithString:self.urlTextField.text];
+    
+    [self loadGivenURL:self.urlTextField.text];
+    
+    return TRUE;
+}
+
+-(void)loadGivenURL:(NSString *)givenURL {
+    
+    NSURL *url = [NSURL URLWithString:givenURL];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [self.variableWebView loadRequest:request];
-    return TRUE;
+    
 }
 @end
